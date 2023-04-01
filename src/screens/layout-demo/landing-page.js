4d38ec249cf2a5d814a072ecf0b1cc1bcd78e2e9
Nguyễn Hiếu Đẹp Trai <46532530+nguyenhieudeptrai@users.noreply.github.com>
2023-04-01 16:29:32 +0700

    Update landing-page.js

--- src/screens/layout-demo/landing-page.js
+++ src/screens/layout-demo/landing-page.js
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { forwardRef, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { NavItem } from './components/NavItem';
 
@@ -18,85 +18,163 @@ const navs = [
 ]
 const TOTAL_PARTS = 5;
 
+const Title = forwardRef(({ }, ref) => {
+    const [index, setIndex] = useState(-1);
+
+    useEffect(() => {
+        if (ref) ref.current = {
+            changeIndex,
+        }
+    }, [ref]);
+
+    const changeIndex = (index) => {
+        setIndex(index);
+    }
+
+    return (
+        <div className={clsx('sticky top-6 mt-6 m-auto z-10 flex justify-center perspective-100',
+        )}>
+            <p className={clsx('absolute top-0 left-1/2 p-2 text-center',
+                'text-2xl border bg-black transition-all ease-in-out duration-700',
+                index < 1 ? "rotate-up" :
+                    index > 1 ? "rotate-down" : "rotate-front"
+            )}>
+                Lorem ipsum dolor sit amet 1
+                <br />
+                <span className='text-sm'>Duis varius interdum lectus</span>
+            </p>
+            <p className={clsx('absolute top-0 left-1/2 p-2 text-center',
+                'text-2xl border bg-black transition-all ease-in-out duration-700',
+                index < 2 ? "rotate-up" :
+                    index > 2 ? "rotate-down" : "rotate-front"
+            )}>
+                Lorem ipsum dolor sit amet 2
+                <br />
+                <span className='text-sm'>Duis varius interdum lectus</span>
+            </p>
+            <p className={clsx('absolute top-0 left-1/2 p-2 text-center',
+                'text-2xl border bg-black transition-all ease-in-out duration-700',
+                index < 3 ? "rotate-up" :
+                    index > 3 ? "rotate-down" : "rotate-front"
+            )}>
+                Lorem ipsum dolor sit amet 3
+                <br />
+                <span className='text-sm'>Duis varius interdum lectus</span>
+            </p>
+            <p className={clsx('absolute top-0 left-1/2 p-2 text-center',
+                'text-2xl border bg-black transition-all ease-in-out duration-700',
+                index < 4 ? "rotate-up" :
+                    index > 4 ? "rotate-down" : "rotate-front"
+            )}>
+                Lorem ipsum dolor sit amet 4
+                <br />
+                <span className='text-sm'>Duis varius interdum lectus</span>
+            </p>
+
+        </div>
+    )
+
+})
+
 export const LandingPage = () => {
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [update, set_] = useState(false);
 
-    const currentPageConifg = useRef({
+    const page = useRef({
         index: 0,
         scrolling: false,
-        fromWheel: false,
-        tmpIndex: 0
+        scrollType: '',
+        idTimeout: 0,
+        tmpIndex: 0,
+        scrollValue: 0
     });
     const mainRef = useRef();
+    const titleRef = useRef();
     useEffect(() => {
         document.title = "Landing Page";
         mainRef.current.addEventListener("wheel", onWheelScroll);
         mainRef.current.addEventListener("scroll", onUserScroll);
-        mainRef.current.addEventListener("mouseup", () => {
-            if (currentPageConifg.current.scrolling && !currentPageConifg.current.fromWheel) {
-                console.log("scroll to", currentPageConifg.current.tmpIndex);
-                // _scrollToPart(null, currentPageConifg.current.tmpIndex);
-            }
-        });
+        mainRef.current.addEventListener("mouseup", onScrolLeave);
+        mainRef.current.addEventListener("touchmove", onTouchMove);
+
         return () => {
-            mainRef.current.removeEventListener("wheel", onWheelScroll);
-            mainRef.current.removeEventListener("scroll", onUserScroll);
+            mainRef.current?.removeEventListener("touchmove", onTouchMove);
+            mainRef.current?.removeEventListener("mouseup", onScrolLeave);
+            mainRef.current?.removeEventListener("wheel", onWheelScroll);
+            mainRef.current?.removeEventListener("scroll", onUserScroll);
         }
     }, []);
 
     const forceUpdate = () => set_(p => !p);
 
-
-    const onUserScroll = () => {
-        if (currentPageConifg.current.fromWheel) return;
-        console.log("onUserScroll");
-        const element = mainRef.current;
-        const realPageSize = Math.floor(element.scrollHeight * 10 / element.offsetHeight) / 10;
-        const HEIGHT = window.innerHeight;
-        const currentY = element.scrollTop;
-        const pageIndex = Math.floor(currentY / HEIGHT);
-        // if (!currentPageConifg.current.scrolling && !currentPageConifg.current.fromWheel) {
-        //     currentPageConifg.current.scrolling = true;
-        //     console.log("start");
-        // }
-        // currentPageConifg.current.tmpIndex = pageIndex;
-        // if (element.scrollTop === 0) {
-        //     console.log('Start of scrolling');
-        // }
-
-        // if (element.scrollTop === element.scrollHeight - element.offsetHeight) {
-        //     console.log('End of scrolling');
-        // }
+    const onTouchMove = () => {
+        page.current.scrollType = "TOUCH";
     }
 
-    const onWheelScroll = (e) => {
-        if (currentPageConifg.current.scrolling) return;
-        const direction = e.deltaY > 0 ? 1 : -1;
-        const prevIndex = currentPageConifg.current.index;
-        if (direction === 1 && currentPageConifg.current.index < TOTAL_PARTS) {
-            currentPageConifg.current.index++;
-            _scrollToPart(prevIndex, currentPageConifg.current.index);
-        } else if (direction === -1 && currentPageConifg.current.index > 0) {
-            currentPageConifg.current.index--;
-            _scrollToPart(prevIndex, currentPageConifg.current.index);
+    const onScrolLeave = () => {
+        if (page.current.scrollType == "USER") {
+            page.current.index = page.current.tmpIndex;
+            _scrollToPart(page.current.tmpIndex);
         }
     }
 
-    const _scrollToPart = (prev, index) => {
-        currentPageConifg.current.scrolling = true;
+    const onUserScroll = ({ target: { scrollHeight, scrollTop, clientHeight } }) => {
+        page.current.tmpIndex = Math.round(scrollTop / clientHeight);
+        titleRef.current.changeIndex(page.current.tmpIndex);
+        if (page.current.scrollType == "WHEEL") {
+            clearTimeout(page.current.idTimeout);
+            page.current.idTimeout = setTimeout(() => {
+                page.current.scrollType = "";
+                page.current.scrolling = false;
+            }, 200);
+            return;
+        };
+        if (page.current.tmpIndex == TOTAL_PARTS - 1 && scrollTop / clientHeight - page.current.tmpIndex > 0.3) {
+            page.current.tmpIndex = TOTAL_PARTS;
+        }
+        if (page.current.scrollType == "TOUCH") {
+            clearTimeout(page.current.idTimeout);
+            page.current.idTimeout = setTimeout(() => {
+                page.current.scrolling = false;
+                page.current.scrollType = "";
+                page.current.index = page.current.tmpIndex;
+                _scrollToPart(page.current.tmpIndex);
+            }, 200);
+            return
+        }
+        page.current.scrollValue = scrollTop;
+        page.current.scrollType = "USER";
+    }
+
+    const onWheelScroll = (e) => {
+        if (page.current.scrolling) return;
+        const direction = e.deltaY > 0 ? 1 : -1;
+        let scrolling = false;
+        if (direction === 1 && page.current.index < TOTAL_PARTS) {
+            page.current.index++;
+            scrolling = true;
+        } else if (direction === -1 && page.current.index > 0) {
+            page.current.index--;
+            scrolling = true;
+        }
+        if (scrolling) {
+            page.current.scrolling = true;
+            page.current.scrollType = "WHEEL";
+            _scrollToPart(page.current.index);
+        }
+    }
+
+    const _scrollToPart = (index) => {
         forceUpdate();
         mainRef.current.scrollTo({
             top: index * window.innerHeight,
             behavior: 'smooth'
         });
-        setTimeout(() => {
-            currentPageConifg.current.scrolling = false;
-        }, 700);
     }
     return (
-        <div className='landing-page-demo bg-black text-white h-screen relative overflow-auto overflow-x-hidden' ref={mainRef}>
+        <div ref={mainRef}
+            className='landing-page-demo bg-black text-white h-screen relative overflow-auto overflow-x-hidden'>
             <header className='flex p-10 absolute w-full z-10'>
                 <div className='flex-1'>
                     <p className='text-4xl'>LOGO</p>
@@ -116,7 +194,7 @@ export const LandingPage = () => {
                 <div className='flex h-screen items-center relative px-6'>
                     <BgImage className='absolute left-1/2 -top-[10%] -translate-x-1/2 w-[120%] h-full stroke-blue-600 blur-[3px]' />
                     <div className={clsx('flex-1 transition-all ease-in-out duration-500',
-                        currentPageConifg.current.index == 0 ? "translate-x-0 opacity-100" : "-translate-x-1/2 opacity-0"
+                        page.current.index == 0 ? "translate-x-0 opacity-100" : "-translate-x-1/2 opacity-0"
                     )}>
                         <p className='text-5xl mb-6 uppercase'>
                             <span className='font-thin font-raleway' >Lorem ipsum</span> <br />
@@ -140,51 +218,12 @@ export const LandingPage = () => {
                         </div>
                     </div>
                     <div className={clsx('flex-1 relative h-full flex items-center justify-center transition-all ease-in-out duration-500',
-                        currentPageConifg.current.index == 0 ? "translate-x-0 opacity-100" : "translate-x-1/2 opacity-0"
+                        page.current.index == 0 ? "translate-x-0 opacity-100" : "translate-x-1/2 opacity-0"
                     )}>
                         <img className='h-2/4 rounded-2xl' src="https://picsum.photos/600/600" />
                     </div>
                 </div>
-                <div className={clsx('sticky top-6 mt-6 m-auto z-10 flex justify-center perspective-100',
-                )}>
-                    <p className={clsx('absolute top-0 left-1/2 p-2 text-center',
-                        'text-2xl border bg-black transition-all ease-in-out duration-700',
-                        currentPageConifg.current.index < 1 ? "rotate-up" :
-                            currentPageConifg.current.index > 1 ? "rotate-down" : "rotate-front"
-                    )}>
-                        Lorem ipsum dolor sit amet 1
-                        <br />
-                        <span className='text-sm'>Duis varius interdum lectus</span>
-                    </p>
-                    <p className={clsx('absolute top-0 left-1/2 p-2 text-center',
-                        'text-2xl border bg-black transition-all ease-in-out duration-700',
-                        currentPageConifg.current.index < 2 ? "rotate-up" :
-                            currentPageConifg.current.index > 2 ? "rotate-down" : "rotate-front"
-                    )}>
-                        Lorem ipsum dolor sit amet 2
-                        <br />
-                        <span className='text-sm'>Duis varius interdum lectus</span>
-                    </p>
-                    <p className={clsx('absolute top-0 left-1/2 p-2 text-center',
-                        'text-2xl border bg-black transition-all ease-in-out duration-700',
-                        currentPageConifg.current.index < 3 ? "rotate-up" :
-                            currentPageConifg.current.index > 3 ? "rotate-down" : "rotate-front"
-                    )}>
-                        Lorem ipsum dolor sit amet 3
-                        <br />
-                        <span className='text-sm'>Duis varius interdum lectus</span>
-                    </p>
-                    <p className={clsx('absolute top-0 left-1/2 p-2 text-center',
-                        'text-2xl border bg-black transition-all ease-in-out duration-700',
-                        currentPageConifg.current.index < 4 ? "rotate-up" :
-                            currentPageConifg.current.index > 4 ? "rotate-down" : "rotate-front"
-                    )}>
-                        Lorem ipsum dolor sit amet 4
-                        <br />
-                        <span className='text-sm'>Duis varius interdum lectus</span>
-                    </p>
-
-                </div>
+                <Title ref={titleRef} />
                 <div className='flex flex-col items-center h-screen relative px-6 '>
                     <div className='flex items-center flex-1'>
                         {Array(4).fill("").map((v, i) => (
@@ -192,14 +231,14 @@ export const LandingPage = () => {
                                 style={{ transitionDelay: 100 * (i + 1) + "ms" }}
                                 className={clsx('flex-1 mx-4 border p-4 h-3/4 rounded-md',
                                     `transition-all ease-in-out duration-700`,
-                                    currentPageConifg.current.index == 1 ? "translate-y-0 opacity-100" :
-                                        currentPageConifg.current.index > 1 ? "-translate-y-1/2 opacity-0" :
+                                    page.current.index == 1 ? "translate-y-0 opacity-100" :
+                                        page.current.index > 1 ? "-translate-y-1/2 opacity-0" :
                                             "translate-y-1/2 opacity-0"
                                 )}
 
                             >
                                 <p className='text-4xl mb-3'>Lorem ipsum dolor sit amet</p>
-                                <KeyTyping className='text-lg' delay={200 * (i + 1)} started={currentPageConifg.current.index == 1}
+                                <KeyTyping className='text-lg' delay={200 * (i + 1)} started={page.current.index == 1}
                                     data="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis varius interdum lectus, vel molestie velit bibendum quis."
                                 />
                             </div>
@@ -208,10 +247,10 @@ export const LandingPage = () => {
                     </div>
                 </div>
                 <div className='flex flex-col items-center justify-center h-screen w-full relative '>
-                    <div className={clsx('transition-all duration-500 delay-500', currentPageConifg.current.index == 2 ? "scale-100 translate-x-8 -translate-y-20" : "scale-75 translate-x-8 -translate-y-0")}>
+                    <div className={clsx('transition-all duration-500 delay-500', page.current.index == 2 ? "scale-100 translate-x-8 -translate-y-20" : "scale-75 translate-x-8 -translate-y-0")}>
                         <div className='tv-bg pl-20 pr-[320px] pt-[270px] pb-28'>
                             <iframe className='flex h-[490px] w-[660px]'
-                                src={"https://www.youtube.com/embed/D1PvIWdJ8xo?rel=0&color=white&fs=0&autoplay=" + (currentPageConifg.current.index == 2 ? "1" : "0")}
+                                src={"https://www.youtube.com/embed/D1PvIWdJ8xo?rel=0&color=white&fs=0&autoplay=" + (page.current.index == 2 ? "1" : "0")}
                                 allow="accelerometer; autoplay; encrypted-media; gyroscope"
                             />
                         </div>
@@ -223,7 +262,7 @@ export const LandingPage = () => {
                             className="relative flex items-center justify-center transition-all ease-in-out duration-700"
                             focusClassName="scale-150 w-[310px] h-[600px]"
                             normaClassName="scale-100 w-[210px] h-[600px]"
-                            started={currentPageConifg.current.index == 3} >
+                            started={page.current.index == 3} >
                             {Array(4).fill("").map((v, i) => (
                                 <div key={i}>
                                     <BoxShape config={{ width: 200, height: 200 }} border={1} color="white" >
@@ -242,7 +281,7 @@ export const LandingPage = () => {
                             width={150}
                             height={200}
                             spaceX={10}
-                            started={currentPageConifg.current.index == 4} >
+                            started={page.current.index == 4} >
                             {Array(6).fill("").map((v, i) => (
                                 <div key={i} className='flex flex-col items-center border border-white bg-black p-2 rounded-lg'>
                                     <img src="https://picsum.photos/200/200" className='h-20 w-20 rounded-full object-cover' />
